492dd92c8fdc31d37950ed77481a420e6e07aa69
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-06-03 16:18:46 +0200

    Add user class

--- src/main/java/com/codecool/onlineshop/model/User.java
+++ src/main/java/com/codecool/onlineshop/model/User.java
@@ -1,4 +1,32 @@
 package com.codecool.onlineshop.model;
 
 public class User {
+    private int userId;
+    private String userName;
+    private String password;
+    private String perrmission;
+
+    public User(int userId, String userName, String password, String perrmission){
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.perrmission = perrmission;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPerrmission() {
+        return perrmission;
+    }
 }
+
