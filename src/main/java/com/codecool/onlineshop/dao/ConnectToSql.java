05b3d1bba62046be5cab5520a7eca395dc139376
Michał Nabielec <michal.nabie@gmail.com>
2019-06-04 11:40:06 +0200

    Added connect to DB

---
+++ src/main/java/com/codecool/onlineshop/dao/ConnectToSql.java
@@ -0,0 +1,34 @@
+package com.codecool.onlineshop.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectToSql {
+    public Connection connection = null;
+
+    public Connection connect() {
+        try {
+            String url = "jdbc:sqlite:src/resources/onlineShop.db";
+
+            connection = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return connection;
+    }
+
+    public void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
