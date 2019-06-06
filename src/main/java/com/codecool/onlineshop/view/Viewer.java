900c5d8e5af7e88e25d504136b52126a3257686f
Pawel <p.j.bieniek@wp.pl>
2019-06-06 11:58:50 +0200

    Add comments to explain Viewer methods

--- src/main/java/com/codecool/onlineshop/view/Viewer.java
+++ src/main/java/com/codecool/onlineshop/view/Viewer.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 public class Viewer {
 
+    /*
+    displayTable merges headers with content and display it in console
+     */
     public void displayTable(List<List<String>> table){
         List<String> headers = new ArrayList<>();
         headers.add("ID");
@@ -24,6 +27,10 @@ public class Viewer {
         System.out.println(content);
     }
 
+    /*
+    productsToString will read List of products
+    and put it to list of lists (each product as a row)
+     */
     public List<List<String>> productsToString(List<Product> products){
         List<List<String>> list = new ArrayList<>();
         for (Product product : products) {
