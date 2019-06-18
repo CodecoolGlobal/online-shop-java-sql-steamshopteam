74f80d22e51b78bbf87982d12465a436682ff09d
Michal Nabielec <michal.nabie@gmail.com>
2019-06-18 10:57:43 +0200

    Add create product and user input(float)

--- src/main/java/com/codecool/onlineshop/controller/ProductController.java
+++ src/main/java/com/codecool/onlineshop/controller/ProductController.java
@@ -14,6 +14,21 @@ public class ProductController {
     public void deactiveEmptyProduct() {
         productService.deactiveProductWhenQuantityEquals0();
         Print.printText("Deactivate empty product");
+    }
+
+    public void createNewProduct() {
+        Print.printText("Product name: ");
+        String name = ReadInput.UserStringInput();
+        Print.printText("Product amount");
+        int amount = ReadInput.UserIntInput();
+        Print.printText("Product isAvailable");
+        int isAvailable = ReadInput.UserIntInput();
+        Print.printText("Product categoryId");
+        int categoryId = ReadInput.UserIntInput();
+        Print.printText("Product price");
+        float price = ReadInput.UserFloatInput();
+
+        new ProductService().create(name, amount, isAvailable, categoryId, price);
 
     }
 }
