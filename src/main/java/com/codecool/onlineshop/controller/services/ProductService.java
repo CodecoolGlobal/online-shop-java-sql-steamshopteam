18b1b03a1c2a11b9ab7816fa25eae654bb7d1e6b
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-06-24 01:42:37 +0200

    Refactor of ProductService

--- src/main/java/com/codecool/onlineshop/controller/services/ProductService.java
+++ src/main/java/com/codecool/onlineshop/controller/services/ProductService.java
@@ -44,12 +44,6 @@ public class ProductService {
         return null;
     }
 
-    private List<Product> getProducts() {
-        List<Product> productList;
-        productList = getAllProducts();
-        return productList;
-    }
-
 
     public void updateProductName(int productId, String newProductName) {
         Product product = getProductById(productId);
@@ -82,7 +76,7 @@ public class ProductService {
     }
 
     public void deactivateProductWhenQuantityEquals0() {
-        List<Product> productList = getProducts();
+        List<Product> productList = getAllProducts();
         for (Product product : productList) {
             if (product.getAmount() == 0) {
                 updateProductIsAvailable(product.getId(), 0);
@@ -91,7 +85,7 @@ public class ProductService {
     }
 
     public void updateProduct(String name, String productName, int amount, int is_available, int categoryId, float price) {
-        List<Product> productList = getProducts();
+        List<Product> productList = getAllProducts();
 
         for (Product product : productList) {
             if (product.getName().equals(name)) {
@@ -118,7 +112,7 @@ public class ProductService {
     }
 
     public void discountProduct(int index, int discount) {
-        List<Product> productList = getProducts();
+        List<Product> productList = getAllProducts();
         float dis = (float) discount / 100;
         for (Product product : productList) {
             if (product.getId() == index) {
@@ -129,15 +123,15 @@ public class ProductService {
 
     }
 
-    public List getProductByCategory(String categoryName){
+    public List getProductByCategory(String categoryName) {
         List<Product> productList = getAllProducts();
         List<Product> productsByCategoryName = new ArrayList<>();
         CategoryService categoryService = new CategoryService();
         List<Category> categoryList = categoryService.readAllCategory();
-        for(Product product : productList){
-            for(Category category : categoryList){
-                if(category.getName().equals(categoryName)){
-                    if(category.getId() == product.getCategoryId()) {
+        for (Product product : productList) {
+            for (Category category : categoryList) {
+                if (category.getName().equals(categoryName)) {
+                    if (category.getId() == product.getCategoryId()) {
                         productsByCategoryName.add(product);
                     }
                 } else {
@@ -149,23 +143,23 @@ public class ProductService {
     }
 
     public List<Product> getAvailableProduct() {
-        List<Product> productList = getProducts();
+        List<Product> productList = getAllProducts();
         List<Product> filteredProduct = new ArrayList<>();
 
-        for(Product product : productList) {
-            if(product.getIsAvailable() == 1)
+        for (Product product : productList) {
+            if (product.getIsAvailable() == 1)
                 filteredProduct.add(product);
         }
         return filteredProduct;
 
     }
 
-    public void deactivateProductOf(int index){
-        List<Product> productList = getProducts();
+    public void deactivateProductOf(int index) {
+        List<Product> productList = getAllProducts();
 
-        for(Product product : productList)
-            if(product.getId() == index) {
-                updateProductIsAvailable(product.getId(),0);
+        for (Product product : productList)
+            if (product.getId() == index) {
+                updateProductIsAvailable(product.getId(), 0);
             }
 
     }
